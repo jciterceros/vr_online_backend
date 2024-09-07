6345e9ff1085a65b2982de0e0f13055d69b20657
jciterceros <jciterceros@yahoo.com>
2024-09-07 11:06:41 -0300

    First commit, create project

---
+++ src/main/java/com/jciterceros/vr_online_backend/VrOnlineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jciterceros.vr_online_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrOnlineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrOnlineBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jciterceros/vr_online_backend/VrOnlineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jciterceros.vr_online_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrOnlineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

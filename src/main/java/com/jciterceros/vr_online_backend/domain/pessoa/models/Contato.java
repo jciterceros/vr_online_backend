f1d69be5f30f5fa92fffaad1dbea2ce3177cff44
jciterceros <jciterceros@yahoo.com>
2024-09-17 02:04:57 -0300

    update: Atualizando Nome ContatoService para Contato e ContatoServiceRepository para ContatoRepository

---
+++ src/main/java/com/jciterceros/vr_online_backend/domain/pessoa/models/Contato.java
@@ -0,0 +1,27 @@
+package com.jciterceros.vr_online_backend.domain.pessoa.models;
+
+import com.jciterceros.vr_online_backend.domain.endereco.models.Endereco;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_contato")
+@Data
+@NoArgsConstructor
+public class Contato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "contato")
+    private List<Endereco> enderecos;
+
+    @OneToMany(mappedBy = "contato")
+    private List<Telefone> telefones;
+
+    @ManyToOne
+    @JoinColumn(name = "pessoa_id")
+    private Pessoa pessoa;
+}
--- src/main/java/com/jciterceros/vr_online_backend/domain/pessoa/repositories/ContatoRepository.java
+++ src/main/java/com/jciterceros/vr_online_backend/domain/pessoa/repositories/ContatoRepository.java
@@ -1,9 +1,9 @@
 package com.jciterceros.vr_online_backend.domain.pessoa.repositories;
 
-import com.jciterceros.vr_online_backend.domain.pessoa.models.ContatoService;
+import com.jciterceros.vr_online_backend.domain.pessoa.models.Contato;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContatoServiceRepository extends JpaRepository<ContatoService, Long>{
+public interface ContatoRepository extends JpaRepository<Contato, Long>{
 }
